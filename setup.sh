5130ba51b0f585ccb07bcbe8a5b1c48fc71b3465
evanrichards <evanrichards@me.com>
2013-07-16 11:46:36 -0500

    Removed screen

--- setup.sh
+++ setup.sh
@@ -39,7 +39,6 @@ if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
 git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
